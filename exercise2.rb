1400f9f282c448aada7fb06b6f7eff45a305bac2
DataNick <DataNick@users.noreply.github.com>
2014-10-21 13:54:40 -0400

    rest of answers for question 2

--- exercise2.rb
+++ exercise2.rb
@@ -1,3 +1,19 @@
+#1
 meal_price = 55
 meal_price_w_tip = ((55*100)*0.15/100)
 puts meal_price_w_tip
+
+#2
+#puts "This string can't add" + 2"
+#this won't work; "no implicit conversion of Fixnum
+#into String (TypeError)
+puts "This string can add" + " 2"
+
+#3
+huge_num = 45628 + 7839
+puts "These numbers are huge and the answer is #{huge_num}."
+
+#4
+#I thought this would be false. Looked at it again and saw the '!'
+#on the last comparison. The answer is true.
+puts (true && false) || (false && true) || !(false && false)
